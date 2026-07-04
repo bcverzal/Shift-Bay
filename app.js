d74aecb3f18421472200690936b7fea29067c550
Brian Verzal <bcver@users.noreply.github.com>
2026-07-04 16:35:23 -0500

    Fix manager access dialog layout

--- app.js
+++ app.js
@@ -10606,6 +10606,7 @@ function wireEvents() {
   $("closeStorageInfoBtn").onclick = () => $("storageInfoDialog").close();
   $("closeRecentActivityBtn")?.addEventListener("click", () => $("recentActivityDialog").close());
   $("closeManagerAccessBtn")?.addEventListener("click", () => $("managerAccessDialog").close());
+  $("closeManagerAccessTopBtn")?.addEventListener("click", () => $("managerAccessDialog").close());
   $("managerInviteForm")?.addEventListener("submit", sendManagerInvite);
   $("pasteEmployeesBtn").onclick = openPasteEmployeesDialog;
   $("revealArchiveAllEmployees").onchange = () => {
